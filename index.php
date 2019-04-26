0e889546eaf0ac5eb633084e9b8f0f4832eccd6c
theweicode <theweicode@gmail.com>
2019-04-26 13:25:27 -0700

    more syntax learned

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php
+$name = "Will";
+$lname = "Ting";
+const test = "test";
+const fullname = "Will Ting";
+const plane = "Jet";
+const nsent = "Hi my name is Will Ting";
 
+function randSent()
+{
+    $rand = str_shuffle(nsent);
+    return $rand;
+}
+
+
+function printName()
+{
+    $car = "BMW";
+    return $car;
+}
+
+function printFullName()
+{
+    return fullname;
+}
 
 ?>
+
+
 
 
 
@@ -11,8 +36,16 @@
 
 
 <body>
-    <h1>My First heading</h1>
-    <p>my first paragraph</p>
+    <?php echo "<h1>My Name is {$name}<h1>" ?>
+    <?php echo $name ?>
+    <!-- php uses . as concatenator -->
+    <?php echo "<h1>My Name is " . $name . "<h1>"
+        . "My plane is a " . plane
+    ?>
+
+    <?php echo printName(); ?>
+    <?php echo "<h1>" . printFullName() . "<h1>"; ?>
+    <?php echo randSent() ?>
 </body>
 
 </html>
